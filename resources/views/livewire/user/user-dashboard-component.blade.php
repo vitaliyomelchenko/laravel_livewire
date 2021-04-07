6e6d158d04cacfd761a434b444a4fd87fba6bf85
vitaliyomelchenko <vitaxa000@gmail.com>
2021-04-07 19:14:52 +0300

    Authentication is done

---
+++ resources/views/livewire/user/user-dashboard-component.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>User Dashboard Page</h1>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Http\Livewire\AboutUsComponent;
+use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\ContactUsComponent;
 use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\ShopComponent;
+use App\Http\Livewire\User\UserDashboardComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,11 +21,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', HomeComponent::class);
 Route::get('/shop', ShopComponent::class);
 Route::get('/cart', CartComponent::class);
 Route::get('/checkout', CheckoutComponent::class);
+Route::get('/about-us', AboutUsComponent::class);
+Route::get('/contact-us', ContactUsComponent::class);
+//Admin
+Route::get('/admin/dashboard', AdminDashboardComponent::class)
+    ->middleware(['auth:sanctum', 'verified', 'authadmin'])
+    ->name('admin.dashboard');
+//User
+Route::get('/user/dashboard', UserDashboardComponent::class)
+    ->middleware(['auth:sanctum', 'verified'])
+    ->name('user.dashboard');
+
+
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+//     return view('dashboard');
+// })->name('dashboard');
+
+
+// Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function(){
+//     Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
+// });
+
+// Route::middleware(['auth:sanctum', 'verified'])->group(function(){
+//     Route::get('/user/dashboard', UserDashboardComponent::class)->name('user.dashboard');
+// });
+
